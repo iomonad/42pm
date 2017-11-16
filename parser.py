31bc853bb4c29fb63427f06e714ba61c52bf6742
Simon CORNAZ <scornaz@e1r13p6.42.fr>
2017-11-16 12:14:11 +0100

    ok

--- parser.py
+++ parser.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 
 import json
+import os
 
 with open('config.json', 'r') as f:
     conf = json.load(f) 
-print json.dumps(conf, indent=4, sort_keys=True)
-
-files = [ f for f in os.listdir( os.curdir ) if os.path.isfile(f) ] #list comprehension version.
+files = [ f for f in os.listdir( os.curdir ) if os.path.isfile(f) ] 
 with open('./Makefile') as infile, open('tmp', 'w') as outfile:
     for line in infile:
-        for src, target in replacements.iteritems():
+        for src, target in conf.iteritems():
             line = line.replace(src, target)
         outfile.write(line)
